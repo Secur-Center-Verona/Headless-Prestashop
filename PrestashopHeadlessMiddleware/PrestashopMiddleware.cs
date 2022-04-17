9a36901d1d5dd5c17e07f7c1ed63f1a188460782
lucamozzo <lucamozzo21173@gmail.com>
2022-04-17 09:53:16 +0100

    Add project files.

---
+++ PrestashopHeadlessMiddleware/PrestashopMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using PrestashopHeadlessMiddleware.Accounts;
+using PrestashopHeadlessMiddleware.Carts;
+using PrestashopHeadlessMiddleware.Categories;
+using PrestashopHeadlessMiddleware.Orders;
+using PrestashopHeadlessMiddleware.Products;
+
+namespace PrestashopHeadlessMiddleware
+{
+    public static class PrestashopMiddleware
+    {
+        private static string _connectionString;
+
+        public static AccountsService AccountsService { get; private set; }
+        public static CartsService CartsService { get; private set; }
+        public static CategoriesService CategoriesService { get; private set; }
+        public static OrdersService OrdersService { get; private set; }
+        public static ProductsService ProductsService { get; private set; }
+
+        public static void Initialize(string databaseHost, string databasePort, string user, string password, string databaseName)
+        {
+            _connectionString = $"server={databaseHost}:{databasePort};user id={user};password={password};Convert Zero Datetime=True;persistsecurityinfo=True;database={databaseName}";
+
+            AccountsService = new AccountsService(_connectionString);
+            CartsService = new CartsService();
+            CategoriesService = new CategoriesService();
+            OrdersService = new OrdersService();
+            ProductsService = new ProductsService();
+        }
+    }
+}
